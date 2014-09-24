ebe13e91c65786d0547615a1affeff0dab4d6564
Takuto Wada <takuto.wada@gmail.com>
2014-09-24 22:41:35 +0900

    test(utf8ize-sourcemaps): do nothing if sourcemap comment is not attached

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@
 delete require.cache[require.resolve('../')];
 var utf8ize = require('../');
 var fs = require('fs');
+var path = require('path');
 var assert = require('assert');
 
 describe('utf8ize', function() {
@@ -13,6 +14,11 @@ describe('utf8ize', function() {
     });
     it('leave sourcemap if already utf8ized', function() {
         var expected = fs.readFileSync('test/expected/example.js', 'utf8');
+        var actual = utf8ize(expected);
+        assert.equal(actual, expected);
+    });
+    it('do nothing if sourcemap comment is not attached', function() {
+        var expected = fs.readFileSync(path.join(__dirname, '..', 'index.js'), 'utf8');
         var actual = utf8ize(expected);
         assert.equal(actual, expected);
     });
